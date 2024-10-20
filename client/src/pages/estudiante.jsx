6d6451b52089772a7f9de1c3d2586190634c8ac2
Roy Andres <rhavila789@gmail.com>
2024-10-20 13:44:38 -0500

    feat: implementación del tema a la pagina de estudiante

--- client/src/pages/estudiante.jsx
+++ client/src/pages/estudiante.jsx
@@ -93,9 +93,11 @@ function Estudiante() {
                     </div>
                 </div>
             </div>
-            <div id="student" className="mx-auto">
-                <div className="flex items-center justify-between">
-                    <h2 className="text-[32px] font-extrabold">Historial Académico</h2>
+
+            <section className="mx-auto">
+                {/* Encabezado historial académico */}
+                <div className="flex items-center justify-between mb-[10px]">
+                    <h2 className="text-h3 font-extrabold">Historial Académico</h2>
                     <div className="space-x-[23px]">
                         <span className="text-[#595353] font-extrabold">Periodo Seleccionado</span>
                         <select name="periodo" id="periodo">
@@ -103,19 +105,21 @@ function Estudiante() {
                         </select>
                     </div>
                 </div>
-                <table className="w-full mt-[17px] text-center">
-                    <thead>
-                        <tr>
-                            <th>Asignatura</th>
-                            <th>Calificación</th>
-                            <th>Promedio por asignatura</th>
-                            <th>Docente</th>
+
+                {/* Tabla de notas */}
+                <table className="w-full text-center mb-[76px]">
+                    <thead className="text-white-2 font-bold">
+                        <tr className="rounded-lg overflow-hidden">
+                            <th className="bg-brand-primary py-6 rounded-s-lg">Asignatura</th>
+                            <th className="bg-brand-primary py-6">Calificación</th>
+                            <th className="bg-brand-primary py-6">Promedio por asignatura</th>
+                            <th className="bg-brand-primary py-6 rounded-e-lg">Docente</th>
                         </tr>
                     </thead>
                     <tbody>
                         {
                             calificaciones.map(({ asignatura, calificacion, docente, promedio }, index) =>
-                                <tr key={index}>
+                                <tr className="even:bg-white-1 odd:bg-white-2 text-sm" key={index}>
                                     <td className="py-[25px] px-[10px]">{asignatura}</td>
                                     <td className="py-[25px] px-[10px]">{calificacion}</td>
                                     <td className="py-[25px] px-[10px]">{promedio}</td>
@@ -125,26 +129,49 @@ function Estudiante() {
                         }
                     </tbody>
                 </table>
-                <div className="mx-auto w-max mt-[17px] mb-[85px]">
-                    <span className="text-[#595353] font-extrabold">Promedio del Periodo</span> 7.8
-                </div>
-                <div className="space-y-[18px]">
-                    <h2 className="font-extrabold text-[32px] text-center">Comentarios de los docentes</h2>
-                    <p className="text-sm text-center max-w-[690px] m-auto">En esta sección encontrará los comentarios y la retroalimentación proporcionada por los docentes.
-                        Aquí podrá revisar observaciones detalladas sobre el desempeño, áreas de mejora y recomendaciones.</p>
-                </div>
-                <div className="mx-auto w-max my-[50px]">
-                    Período Seleccionado
-                    <select name="periodo" id="periodo">
-                        <option value="2024-3">2024-3</option>
-                    </select>
-                </div>
-                <div className="grid grid-cols-2 gap-[49px]">
-                    {
-                        comments.map((comment, index) => <CommentCard key={index} {...comment} />)
-                    }
-                </div>
-            </div>
+
+                {/* Promedio del periodo */}
+                <section className="mx-auto w-max rounded-lg border-brand-primary border py-6 px-8 mb-20">
+                    <span className="text-brand-primary font-bold text-lg">Promedio del Periodo</span> 9.3
+                </section>
+
+                {/* Encabezado de los comentarios */}
+                <section className="space-y-4 pt-20 pb-10">
+                    <h2 className="text-h3 font-bold text-center">
+                        Comentarios de los docentes
+                    </h2>
+                    <p className="text-sm text-center max-w-[592px] m-auto">
+                        En esta sección encontrará los comentarios y la retroalimentación proporcionada por los docentes.
+                        Aquí podrá revisar observaciones detalladas sobre el desempeño, áreas de mejora y recomendaciones.
+                    </p>
+                </section>
+
+                {/* Sección de comentarios */}
+                <section className="py-8">
+
+                    {/* Dropdown periodo escolar */}
+                    <form className="mx-auto w-max my-[50px] mb-14 bg-brand-primary text-white-2 text-lg font-bold rounded-lg">
+                        <select className="bg-transparent py-6 px-8 w-full outline-none" name="periodoComentarios" id="periodoComentarios">
+                            <option className="w-full" value="2024-3">
+                                2024-3
+                            </option>
+                            <option className="w-full" value="2024-2">
+                                2024-2
+                            </option>
+                            <option className="w-full" value="2024-1">
+                                2024-1
+                            </option>
+                        </select>
+                    </form>
+
+                    {/* Lista de comentarios */}
+                    <div className="grid grid-cols-2 gap-[49px]">
+                        {
+                            comments.map((comment, index) => <CommentCard key={index} {...comment} />)
+                        }
+                    </div>
+                </section>
+            </section>
         </div>
     )
 }
